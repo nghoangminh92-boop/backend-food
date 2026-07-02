a949d3288539df82149d2c86553fd529dbf31d87
hoangminh <nghoangminh92@gmail.com>
2026-07-02 19:27:09 +0900

    add role to jwt validate

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,6 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
 validate(payload: any) {
-  return { userId: payload.sub, email: payload.email, fullName: payload.fullName, avatar: payload.avatar };
+  return { userId: payload.sub, email: payload.email, fullName: payload.fullName, avatar: payload.avatar, role: payload.role };
 }
 }
